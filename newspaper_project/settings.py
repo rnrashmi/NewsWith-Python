11747898f562bbcc84b31aa597b3994fdaf80e48
Rashmi <rashmirn.sahoo@gmail.com>
2021-11-07 10:12:54 +0530

    admin css issues fix

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#qsx=g1pxq9g)s^($vx)rr)rbu%sk*vi@&hm#+ijqf+=muo78m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -133,6 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+STATIC_ROOT=os.path.join(BASE_DIR,'assests')
+
 
 AUTH_USER_MODEL='user.CustomUser'
 
