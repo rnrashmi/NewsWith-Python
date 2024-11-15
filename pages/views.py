6add2536dc60ffeb38542a86bc9bdfed6f5fa553
Rashmi ranjan <57034465+rnrashmi@users.noreply.github.com>
2024-11-15 23:11:27 +0530

    Update views.py added a comment

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.views.generic import TemplateView
 
+# HomePage rendering view
 class HomeTemplateView(TemplateView):
     template_name='home.html'
 
